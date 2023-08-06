0c7a572b0d8523161ef98069157d3b1034735087
kouji_kawamura <45913424+kouji0705@users.noreply.github.com>
2023-08-06 17:43:50 +0900

    fix:

--- test/sample/sample_test.go
+++ test/sample/sample_test.go
@@ -1,6 +1,7 @@
 package sample
 
 import (
+	"fmt"
 	"testing"
 
 	pr "github.com/kouji0705/go-test/product"
@@ -17,16 +18,24 @@ func TestAdd(t *testing.T) {
 		expected int
 	}{
 		{
-			name: "1+1",
+			name: "1+1(成功)",
 			args: args{1, 1},
 			expected: 2,
+		},
+		{
+			name: "1+1(失敗)",
+			args: args{1, 1},
+			expected: 3,
 		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if actual := pr.Add(tt.args.a, tt.args.b); actual != tt.expected {
+				fmt.Println("テスト失敗", tt.name)
 				t.Errorf("実行値と期待値が異なります。実行値: %v, 期待値: %v", actual, tt.expected)
+			}else {
+				fmt.Println("テスト成功", tt.name)
 			}
 		})
 	}
